19584ec7faaf201b2637de92f74a83304c728251
Juzo(-.-) <alexs22221@gmail.com>
2025-02-09 19:43:29 +0300

    text db to json

--- bot/util/my_db.py
+++ bot/util/my_db.py
@@ -1,29 +1,55 @@
 import os 
+import json
 from typing import List, Optional
+from collections import OrderedDict
 
 from dotenv import dotenv_values
-import aiofiles
 import asyncio
 
-class DB:
-    FILE_NAME = '/' + dotenv_values(".env").get('FILE_NAME') + '.txt'
-    path = os.getcwd() + FILE_NAME
 
+class JsonDB:
+    FILE_NAME = '/' + dotenv_values(".env").get('FILE_NAME') + '.json'
+    path = os.getcwd() + FILE_NAME
+    
 
     @classmethod
-    async def get_letters(cls) -> Optional[List[str]]:
-        """Получение предложенний из файла"""
+    def get_dict_from_db(cls) -> Optional[dict]:
         file_content = None
 
-        async with aiofiles.open(cls.path, "r", encoding="utf-8") as file:
-            file_content = await file.read()
+        with open(cls.path, "r", encoding="utf-8") as file:
+            file_content = OrderedDict(json.load(file))
 
-        return file_content.split(';')[:-1]
-        
+        return file_content
+    
 
     @classmethod
-    async def write_letter(cls, letter: str) -> None:
-        """Запись предложения в файл"""
+    def set_letter(cls, letter: str, answer: str) -> None:
+        data = {}
 
-        async with aiofiles.open(cls.path, "a", encoding="utf-8") as file:
-            await file.write(letter + ';')
+        with open(cls.path, "r", encoding="utf-8") as file:
+            data = OrderedDict(json.load(file))
+
+        data.update({letter: answer})
+
+        with open(cls.path, "w", encoding="utf-8") as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
+
+
+    @classmethod
+    def delete_letter(cls, id: int) -> None:
+        data = None 
+
+        with open(cls.path, "r", encoding="utf-8") as file:
+            data = OrderedDict(json.load(file))
+        
+        for idx, key in enumerate(data.keys()):
+            if idx == id:
+                del data[key]
+
+        with open(cls.path, "w", encoding="utf-8") as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
+        
+
+    @staticmethod
+    def dict_to_str(data: dict) -> List[str]:
+        return [f'{key}: {value}' for key, value in data.items()]
