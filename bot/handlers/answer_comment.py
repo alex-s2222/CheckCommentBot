e441a9ce9bc8662be801addccc5dcbbdc184f186
Juzo(-.-) <alexs22221@gmail.com>
2025-02-17 00:05:06 +0300

    add: check db for answer

--- bot/handlers/answer_comment.py
+++ bot/handlers/answer_comment.py
@@ -1,6 +1,11 @@
 from telegram import Update
 from telegram.ext import CallbackContext
 
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+
+from util.my_db import JsonDB
+
 
 class Comment:
     @staticmethod
@@ -8,5 +13,23 @@ class Comment:
         # Проверяем, является ли сообщение комментарием
         if update.message.reply_to_message:
             original_message = update.message.text
-            response = f'Спасибо за ваш комментарий на сообщение: "{original_message}"'
+            answer = Comment.__get_answer(original_message)
+            if not answer:
+                return None
+            
+            response = f'{answer}'
             await update.message.reply_text(response)
+
+    @staticmethod
+    def __get_answer(user_message: str):
+        db_data = JsonDB.get_dict_from_db()
+        if not db_data:
+            return None
+
+        #WRatio второй способ
+        #token_set_ratio
+        best_match, score = process.extractOne(user_message, db_data.keys(), scorer=fuzz.WRatio)
+        if score < 80:
+            return None 
+        
+        return db_data[best_match] 
